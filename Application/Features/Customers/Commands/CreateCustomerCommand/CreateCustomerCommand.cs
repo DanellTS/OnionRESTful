0a67323b343df4687466784ae839466dbfc90a85
DanellTS <danell.ts@gmail.com>
2022-06-14 23:31:13 -0400

    First Entity

---
+++ Application/Features/Customers/Commands/CreateCustomerCommand/CreateCustomerCommand.cs
@@ -0,0 +1,28 @@
+﻿using Application.Wrappers;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Features.Customers.Commands.CreateCustomerCommand
+{
+    public class CreateCustomerCommand : IRequest<Response<int>>
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateBirth { get; set; }
+        public string Telephone { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+    public class CreateCustomerCommandHandler : IRequestHandler<CreateCustomerCommand, Response<int>>
+    {
+        public async Task<Response<int>> Handle(CreateCustomerCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
